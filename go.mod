a2c0dd708727c6f9243c9903f07f7ac6b2b4c1a9
chinton1234 <98258863+chinton1234@users.noreply.github.com>
2022-11-19 23:27:57 +0700

    add service registry

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module example/CUAccountService
 
 go 1.19
 
+require github.com/hashicorp/consul/api v1.15.3
+
 require (
+	github.com/armon/go-metrics v0.3.10 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/fatih/color v1.9.0 // indirect
 	github.com/fsnotify/fsnotify v1.4.9 // indirect
@@ -13,6 +16,12 @@ require (
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.11.1 // indirect
 	github.com/goccy/go-json v0.9.11 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.1 // indirect
+	github.com/hashicorp/go-hclog v0.14.1 // indirect
+	github.com/hashicorp/go-immutable-radix v1.3.0 // indirect
+	github.com/hashicorp/go-rootcerts v1.0.2 // indirect
+	github.com/hashicorp/golang-lru v0.5.4 // indirect
+	github.com/hashicorp/serf v0.9.7 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgconn v1.13.0 // indirect
 	github.com/jackc/pgio v1.0.0 // indirect
@@ -31,6 +40,8 @@ require (
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mattn/go-colorable v0.1.6 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/mitchellh/mapstructure v1.4.1 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.5 // indirect
--- main.go
+++ main.go
@@ -3,18 +3,56 @@ package main
 import (
 	"example/CUAccountService/controllers"
 	"example/CUAccountService/initializers"
+	"fmt"
+	"os"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 
 	/////
 	"example/CUAccountService/middleware"
 	"example/CUAccountService/service"
+	"log"
 	"net/http"
+
+	consulapi "github.com/hashicorp/consul/api"
 )
 
 func init() {
 	initializers.LoadEnvVariables()
 	initializers.ConnectToDB()
+}
+
+func serviceRegistryWithConsul() {
+	config := consulapi.DefaultConfig()
+	consul, err := consulapi.NewClient(config)
+	if err != nil {
+		log.Println(err)
+	}
+
+	serviceID := "account-service2"
+	port, _ := strconv.Atoi(getPort()[1:len(getPort())])
+	address := "192.168.0.101"
+
+	registration := &consulapi.AgentServiceRegistration{
+		ID:      serviceID,
+		Name:    "account-service",
+		Port:    port,
+		Address: getHostname(),
+		Check: &consulapi.AgentServiceCheck{
+			HTTP:     fmt.Sprintf("http://%s:%v/check", address, port),
+			Interval: "10s",
+			Timeout:  "30s",
+		},
+	}
+
+	regiErr := consul.Agent().ServiceRegister(registration)
+
+	if regiErr != nil {
+		log.Printf("Failed to register service: %s:%v ", address, port)
+	} else {
+		log.Printf("successfully register service: %s:%v", address, port)
+	}
 }
 
 func CORSMiddleware() gin.HandlerFunc {
@@ -33,10 +71,29 @@ func CORSMiddleware() gin.HandlerFunc {
 		c.Next()
 	}
 }
+func check(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Consul check")
+}
+
+func getPort() (port string) {
+	port = os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "8080"
+	}
+	port = ":" + port
+	return
+}
+
+func getHostname() (hostname string) {
+	hostname, _ = os.Hostname()
+	return
+}
 
 func main() {
 	r := gin.Default() //router
 	r.Use(CORSMiddleware())
+	serviceRegistryWithConsul()
 	r.POST("/user", controllers.UserCreate)
 	r.GET("/users", controllers.GetUsers)
 	r.GET("/user/:sid", controllers.GetUserById)
@@ -57,7 +114,12 @@ func main() {
 			ctx.JSON(http.StatusUnauthorized, nil)
 		}
 	})
+	r.GET("/check", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"check": "ok",
+		})
 
+	})
 	v1 := r.Group("/v1")
 	v1.Use(middleware.AuthorizeJWT())
 	{
